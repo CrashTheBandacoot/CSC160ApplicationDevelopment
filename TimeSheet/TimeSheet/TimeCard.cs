f0afcabcf82a9d91cef9d988c77588a7ff8459bf
klee <knlee6@gmail.com>
2016-06-30 23:19:11 -0600

    Added TestStartsOnSunday, this test passes

--- TimeSheet/TimeSheet/TimeCard.cs
+++ TimeSheet/TimeSheet/TimeCard.cs
@@ -14,9 +14,16 @@ namespace TimeSheet
         public Day[] PayPeriod = new Day[numOfDaysInPayPeriod];
         public TimeCard(DateTime payPeriodStart)
         {
-            for(int i = 0; i < numOfDaysInPayPeriod; ++i)
+            if (payPeriodStart.DayOfWeek.Equals(DayOfWeek.Sunday))
             {
-                PayPeriod[i] = new Day(payPeriodStart.AddDays(i));
+                for (int i = 0; i < numOfDaysInPayPeriod; ++i)
+                {
+                    PayPeriod[i] = new Day(payPeriodStart.AddDays(i));
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("payPeriodStart", "payPeriodStart must be a Sunday.");
             }
         }
     }
--- TimeSheet/TimeSheetTests/TimeCardTest.cs
+++ TimeSheet/TimeSheetTests/TimeCardTest.cs
@@ -10,13 +10,26 @@ namespace TimeSheetTests
         [TestMethod]
         public void Test2WeekPayPeriod()
         {
-            DateTime startingDay = new DateTime(2016, 6, 30);
+            DateTime startingDay = new DateTime(2016, 7, 3);
             TimeCard t = new TimeCard(startingDay);
             int arraySize = 0;
             for(int i = 0; i < t.PayPeriod.Length; i++)
             {
                 Assert.AreEqual(t.PayPeriod[i].dateTime, startingDay.AddDays(i));
                 ++arraySize;
+            }
+        }
+
+        [TestMethod]
+        public void TestStartsOnSunday()
+        {
+            try {
+                DateTime startingDay = new DateTime(2016, 6, 30);
+                TimeCard t = new TimeCard(startingDay);
+            }
+            catch(Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentOutOfRangeException));
             }
         }
     }
