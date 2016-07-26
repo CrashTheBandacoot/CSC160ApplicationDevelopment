11668987977c117d8c8a3a10e60938a348df42cc
klee <knlee6@gmail.com>
2016-07-26 08:13:26 -0600

    Adding MeeCreator lab

---
+++ GameLibrary/2DGameLibrary/Playable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DGameLibrary
+{
+    public interface Playable
+    {
+        void TakeTurn(_2DGameLibrary.Player activePlayer);
+    }
+}
