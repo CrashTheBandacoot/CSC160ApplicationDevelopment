f8f56cba36979ea069bf9d4b367ea80e6b7f20b7
klee <knlee6@gmail.com>
2016-07-29 23:00:36 -0600

    Finished

--- MeeCreator/MeeCreator/MainWindow.xaml.cs
+++ MeeCreator/MeeCreator/MainWindow.xaml.cs
@@ -33,5 +33,58 @@ namespace MeeCreator
             myEllipse.Fill = Brushes.Fuchsia;
             FaceCanvas.Children.Add(myEllipse);
         }
+
+        private void b_blue_Click(object sender, RoutedEventArgs e)
+        {
+            e_leftEye.Fill = Brushes.Aqua;
+            e_rightEye.Fill = Brushes.Aqua;
+        }
+
+        private void b_green_Click(object sender, RoutedEventArgs e)
+        {
+            e_leftEye.Fill = Brushes.Chartreuse;
+            e_rightEye.Fill = Brushes.Chartreuse;
+        }
+
+        private void b_brown_Click(object sender, RoutedEventArgs e)
+        {
+            e_leftEye.Fill = Brushes.Chocolate;
+            e_rightEye.Fill = Brushes.Chocolate;
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+        public class InvertValueConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if (value is double)
+                {
+                    double temp = (double)value;
+                    temp *= -1;
+                    value = temp;
+                }
+                return value;
+            }
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if (value is double)
+                {
+                    double temp = (double)value;
+                    temp *= -1;
+                    value = temp;
+                }
+                return value;
+            }
+        }
 }
